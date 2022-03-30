f3f8014685eb006505583819f37920df65c901ab
Giozatto <90328913+Giozatto@users.noreply.github.com>
2022-03-30 21:56:46 +0300

    Add files via upload

---
+++ 6. Основы JavaScript/3.arrays/task.js
@@ -0,0 +1,19 @@
+//Задача 1
+
+function compareArrays(arr1, arr2) {
+
+
+  let result = (arr1.length == arr2.length) && arr1.every((item, index) =>
+    item === arr2[index]);
+  return result;
+}
+compareArrays([8, 9], [8, 9])
+
+
+// Задача 2
+
+function advancedFilter(arr) {
+  let resultArr = arr.filter(item => item > 0).filter(item => item % 3 === 0).map(item => item * 10);
+
+  return resultArr;
+}
